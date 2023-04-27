00af2ea28c90ac0bdee8580f3f2d6c0228094749
william-oliveira-sena <92284934+william-oliveira-sena@users.noreply.github.com>
2023-04-26 23:44:46 -0300

    continuação projeto agenda - banco/home/css

--- 17_Projeto_Agenda/index.php
+++ 17_Projeto_Agenda/index.php
@@ -1,27 +1,48 @@
 <?php
-
-    include_once("config/url.php");
+    include_once("templates/header.php");
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Agenda de Contatos</title>
-        <!--bootstrap-->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/css/bootstrap.min.css" integrity="sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <div class="container">
+  
+        <?php if(isset($printMsg) && $printMsg != ''): ?>
+             <p id="msg"><?= $printMsg ?></p>
+        <?php endif; ?>
+        <h1 id="main-title">Minha Agenda</h1>
+        <?php if(count($contacts) > 0): ?>
+            <table class="table" id="contacts-table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Contato</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
 
-    <!--font-->
+                <tbody>
+                    <?php foreach($contacts as $contact): ?>
+                    <tr>
+                        <td scope="row" class="col-id"><?= $contact['id'] ?></td>
+                        <td scope="row"><?= $contact['name'] ?></td>
+                        <td scope="row"><?= $contact['phone'] ?></td>
+                        <td class="actions">
+                            <a href="a"><i class="fas fa-eye check-icon"></i></a>
+                            <a href="a"><i class="far fa-edit edit-icon"></i></a>
+                            <button type="submit" class="delete-btn"><i class="fas fa-times delete-icon"></i> </button>
+                        </td> 
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- css-->
-    <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
+            </table>
+        <?php else: ?>
 
-</head>
-<body>
-    <h1>testendo agenda</h1>
-    <i class="fas fa-eye"></i>
-</body>
-</html>
+            <p id="empty-list-text">Ainda não há contatos na sua agenda, <a href="<?= $BASE_URL ?>create.php">Clique aqui para Adicionar</a></p>
+        
+        <?php endif; ?>
+    </div>
+
+<?php
+
+    include_once("templates/footer.php");
+?>
