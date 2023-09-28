bf2c2d354489e4f236a141cac70c33b871246805
william-oliveira-sena <92284934+william-oliveira-sena@users.noreply.github.com>
2023-09-27 22:42:13 -0300

    contruindo projeto moviestar

---
+++ 19_MovieStar/templates/index.php
@@ -0,0 +1,76 @@
+<?php
+    require_once("globals.php");
+    require_once("db.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MovieStar</title>
+     <link rel="short icon" href="<?= $BASE_URL ?>img/moviestar.ico">
+     <!-- bootstrap-->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/css/bootstrap.css" integrity="sha512-r0fo0kMK8myZfuKWk9b6yY8azUnHCPhgNz/uWDl2rtMdWJlk7gmd9socvGZdZzICwAkMgfTkVrplDahQ07Gi0A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+     <!--fonttawsen-->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+     <!--css do projeto-->
+     <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+    <header>
+        <nav id="main-navbar" class="navbar navbar-expand-lg">
+            <a href="<?= $BASE_URL ?>" class="navbar-brand">
+               <img src="../img/logo.svg" alt="MovieStar" id="logo">
+               <span id="moviestar-title">MovieStar</span>
+            </a>
+            <buttton class="navbar-toggler" type="button" data-togle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Togglenavigation">
+                <i class="fas fa-bars"></i>
+            </buttton>
+            <form action="" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
+                <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
+                <button class="btn my-2 my-sm-0" type="submit"> 
+                    <i class="fas fa-search"></i>
+                </button>
+            </form>
+             <div class="collapse navbar-collapse" id="navbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="<?= $BASE_URL ?>auth.php" class="nav-link">Entrar/Cadastrar</a>
+                    </li>
+                </ul>
+             </div>
+        </nav>
+
+    </header>
+    <div id="main-container" class="container-fluid">
+        <h1> Corpo do site</h1>
+    </div>
+    <footer id="footer">
+    <div id="social-container">
+        <ul>
+            <li>
+                <a href="#"><i class="fab fa-facebook-square"></i></a>
+            </li>
+            <li>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+            </li>
+            <li>
+                <a href="#"><i class="fab fa-youtube"></i></a>
+            </li>
+        </ul>
+    </div>
+    <div id="footer-links-container">
+    <ul>
+        <li><a href="#">Adicionar filme</a></li>
+        <li><a href="#">Adicionar críticas</a></li>
+        <li><a href="#">Entrar / Registrar</a></li>
+    </ul>
+    </div>
+    <p>&copy; 2023 Hora de Codar</p>
+    </footer>
+
+    <!--bootstrap js-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/js/bootstrap.js" integrity="sha512-ipBeSMCnlAvS4AEbycy0nTk9KkYr5lUJwFHNvf4IxtV/CDW4qx53mZKUryWtNr6GFaBl11EXyrU6iE3mo6ib2g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+</html>
