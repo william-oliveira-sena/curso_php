96781737696017710c832e2f3903cc3ca31eddea
william-oliveira-sena <92284934+william-oliveira-sena@users.noreply.github.com>
2024-01-08 19:25:31 -0300

    adicionando funcionalidades

--- 19_MovieStar/dao/UserDAO.php
+++ 19_MovieStar/dao/UserDAO.php
@@ -60,6 +60,21 @@ class UserDAO implements UserDAOInterface{
         }      
         public function verifyToken($protected = false){
 
+            if(!empty($_SESSION["token"])){
+                //Pega o token da session
+                $token = $_SESSION["token"];
+
+                $user = $this->findByToken($token);
+
+                if($user){
+                    return $user;
+                }else{
+                    $this->message->setMessage("realize a autenticação para acesar esta página","erorr","index.php");
+                }
+            }else{
+                return false;
+            }
+
         }
         public function setTokenToSession($token, $redirect = true){
 
@@ -104,6 +119,27 @@ class UserDAO implements UserDAOInterface{
 
         }
         public function findByToken($token){
+
+            if($token != ""){
+                $stmt = $this->conn->prepare("SELECT * FROM users where token = :token");
+
+                $stmt->bindParam(":token", $token);
+
+                $stmt->execute();
+
+                if($stmt->rowCount() > 0){
+
+                    $data = $stmt->fetch();
+                    $user = $this->buildUser($data);
+
+                    return $user;
+
+                }else{
+                return false;
+                }
+            }else{
+                return false;
+            }
 
         }
         public function changePassword(User $user){
--- 19_MovieStar/templates/header.php
+++ 19_MovieStar/templates/header.php
@@ -1,16 +1,22 @@
 <?php
+
     require_once("globals.php");
     require_once("db.php");
     require_once("models/Message.php");
+    require_once("dao/UserDAO.php");
 
     $message = new Message($BASE_URL);
 
     $flassMessage = $message->getMessage();
 
-    if(!empty($flassMessage["msg"])){
+    if(!empty($flassMessage["msg"])) {
         //limpar a mensagem
         $message->clearMessage();
     }
+
+      $userDao = new UserDAO($conn, $BASE_URL);
+
+      $userData = $userDao->verifyToken(false);
 
 ?>
 <!DOCTYPE html>
@@ -45,9 +51,25 @@
             </form>
              <div class="collapse navbar-collapse" id="navbar">
                 <ul class="navbar-nav">
-                    <li class="nav-item">
+                    <?php if($userData): ?>
+                        <li class="nav-item">
+                        <a href="<?= $BASE_URL ?>newmovie.php" class="nav-link"><i class="far fa-plus-square"></i>incluir filme</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="<?= $BASE_URL ?>dashboard.php" class="nav-link">Meus filmes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="<?= $BASE_URL ?>editprofile.php" class="nav-link bold"><?= $userData->name ?></a>
+                        </li>
+                     
+                        <li class="nav-item">
+                            <a href="<?= $BASE_URL ?>logout.php" class="nav-link">Sair</a>
+                        </li>
+                    <?php else: ?>
+                        <li class="nav-item">
                         <a href="<?= $BASE_URL ?>auth.php" class="nav-link">Entrar/Cadastrar</a>
                     </li>
+                    <?php endif; ?>
                 </ul>
              </div>
         </nav>
